761939b7552b5c30acc8f69d2ddd92b4da3cbe3e
Kirat Virk <kirat_virk3@yahoo.ca>
2017-02-21 18:48:23 +0000

    some refactoring

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var length;
   $(".container textarea").keyup(function(){
       length = 140 - $(this).val().length;
-      var $select = $(this).parent().children('.counter');
+      var $select = $(this).siblings('.counter');
       $select.text(length);
       if(length < 0){
         $select.css("color", "red");
